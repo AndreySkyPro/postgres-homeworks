dddfb2ad35386e11e74badfc41c51544b9c984dd
Andrey <bmw_740@mail.ru>
2023-07-15 22:38:10 +0500

    hw1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees(
+	first_name varchar(255),
+	last_name varchar(255),
+	title varchar(255),
+	birth_date date,
+	notes text,
+	employees_id serial PRIMARY KEY
+);
+
+CREATE TABLE customers(
+	customer_id varchar(255) PRIMARY KEY,
+	company_name varchar(255),
+	contact_name varchar(255)
+);
+
+CREATE TABLE orders(
+	order_id int PRIMARY KEY,
+	customer_id varchar(255) REFERENCES customers(customer_id),
+	employee_id smallserial REFERENCES employees(employees_id),
+	order_date date,
+	ship_city varchar(255)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+
+
+def insert_tables(file, insert_param, tabl_name):
+    conn = psycopg2.connect(host='localhost', database='north', user='postgres', password='71806')
+    try:
+        with conn.cursor() as cur:
+            with open(file, newline='', encoding='cp1251') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for read in reader:
+                    cur.execute(f'INSERT INTO {tabl_name} VALUES ({insert_param})', read)
+                    conn.commit()
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+
+    file_customers_data = 'north_data/customers_data.csv'
+    tabl_name_customers = 'customers'
+    insert_param_customers_data = "%(customer_id)s, %(company_name)s, %(contact_name)s"
+    insert_tables(file_customers_data, insert_param_customers_data, tabl_name_customers)
+
+    file_employees_data = 'north_data/employees_data.csv'
+    tabl_name_employees = 'employees'
+    insert_param_employees_data = "%(first_name)s, %(last_name)s, %(title)s, %(birth_date)s, %(notes)s"
+    insert_tables(file_employees_data, insert_param_employees_data, tabl_name_employees)
+
+    file_orders_data = 'north_data/orders_data.csv'
+    tabl_name_orders = 'orders'
+    insert_param_orders_data = "%(order_id)s, %(customer_id)s, %(employee_id)s, %(order_date)s, %(ship_city)s"
+    insert_tables(file_orders_data, insert_param_orders_data, tabl_name_orders)
